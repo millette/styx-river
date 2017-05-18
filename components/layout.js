feddf2ccad17d210984ac896dd0d954068709ee1
Robin Millette <robin@millette.info>
2017-05-17 20:44:28 -0400

    Satisfy linter.

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import Head from 'next/head'
 export default ({ children, title = 'This is the default title' }) => (
   <div>
     <Head>
-      <title>{ title }</title>
+      <title> OILQ { title }</title>
       <meta charSet='utf-8' />
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
     </Head>
--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,7 @@
-import React from 'react'
-import Link from 'next/link'
-import fetch from 'isomorphic-fetch'
 import Layout from '../components/layout'
 
 export default () => (
-  <Layout title="About">
+  <Layout title='About'>
     <h1>About</h1>
     <p>bla bla bla...</p>
   </Layout>
