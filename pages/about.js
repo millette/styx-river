da48cdc454f3872814287446cc5d0d762e48a3d2
Robin Millette <robin@millette.info>
2017-05-17 21:18:00 -0400

    Link to sources from about page.

--- pages/about.js
+++ pages/about.js
@@ -3,5 +3,10 @@ import Layout from '../components/layout'
 export default () =>
   <Layout title='About'>
     <h1>About</h1>
-    <p>bla bla bla...</p>
+    <p>Observatoire de l'informatique libre québécois</p>
+    <h2>Sources</h2>
+    <ul>
+      <li>Front-end: <a href='https://github.com/millette/styx-river'>styx-river</a></li>
+      <li>Back-end: <a href='https://github.com/millette/styx-ferryman'>styx-ferryman</a></li>
+    </ul>
   </Layout>
