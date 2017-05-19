447686fd785e262a6c12125f9a2e44c5cb8ca067
Robin Millette <robin@millette.info>
2017-05-18 20:43:05 -0400

    Sources, v2.

---
+++ components/feed-summary.js
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+
+export default ({ it, item }) => {
+  const lang = it.meta && it.meta.language && <small>({it.meta.language})</small>
+  const by = it.meta && it.meta.author && <h4>By {it.meta.author}</h4>
+  const gen = it.meta && it.meta.generator && <p>Generator: {it.meta.generator}</p>
+
+  // console.log(it)
+  return <div>
+    <h3>{it.meta && it.meta.title || item} {lang}</h3>
+    {it.meta && it.meta.image && <img style={{float: 'left'}} src={it.meta.image.url} />}
+    {by}
+    <p><b>{it.error}</b></p>
+    <p>{(it.meta && it.meta.date) || (it.meta && it.meta.pubdate)}</p>
+    <p>{it.meta && it.meta.link}</p>
+    <p>{it.meta && it.meta.description}</p>
+    {gen}
+  </div>
+}
---
+++ pages/sources-v2.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import Link from 'next/link'
+import fetch from 'isomorphic-fetch'
+import FeedSummary from '../components/feed-summary'
+import Layout from '../components/layout'
+
+const perPage = 10
+
+const pad0 = (n) => n.length === 1 ? ('0' + n) : n
+
+export default class MyPage extends React.Component {
+  static async getInitialProps (oy) {
+    const q = oy.query
+    // let u = `https://millette.cloudant.com/u2/_design/itemsDateTitlesWithGUID/_view/items?reduce=false&descending=true&limit=${perPage + 1}&stale=update_after`
+    let u = `http://localhost:5993/u2/_design/FeedsDatesV2/_view/feeds?reduce=false&limit=${perPage + 1}&descending=true&stale=update_after`
+
+    const startWhen = []
+    if (q && q.year) {
+      startWhen.push(pad0(q.year))
+      if (q.month) {
+        startWhen.push(pad0(q.month))
+        if (q.day) {
+          startWhen.push(pad0(q.day))
+          if (q.id) {
+            u += '&startkey_docid=' + encodeURIComponent(q.id)
+          }
+        }
+      }
+      u += '&startkey=' + JSON.stringify(startWhen)
+    }
+
+    const res = await fetch(u)
+    const data = await res.json()
+    const rows = data.rows
+    return { rows }
+  }
+
+  render () {
+    const nextPage = this.props.rows[perPage]
+    // console.log('NP:', nextPage)
+    const query = nextPage && {
+      year: nextPage.key[0],
+      month: nextPage.key[1],
+      day: nextPage.key[2],
+      id: nextPage.id
+    }
+
+    return (
+      <Layout>
+        <h1>Sources V2</h1>
+        {query && <Link prefetch href={{ pathname: '/sources-v2', query }}><a>Next page</a></Link>}
+        {this.props.rows.slice(0, perPage).map((row) => {
+          const it = row.value
+          const item = row.id
+          return (
+            <FeedSummary it={it} key={item} item={item} />
+          )
+        })}
+        {query && <Link prefetch href={{ pathname: '/sources-v2', query }}><a>Next page</a></Link>}
+      </Layout>
+    )
+  }
+}
